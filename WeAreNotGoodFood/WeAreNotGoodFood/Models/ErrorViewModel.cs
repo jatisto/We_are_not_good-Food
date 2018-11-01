be97287d920351f57fc8bcc40f8969a0db53fa93
Jatisto <j.atisto@gmail.com>
2018-11-01 16:37:56 +0600

    Clear project

---
+++ WeAreNotGoodFood/WeAreNotGoodFood/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeAreNotGoodFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
