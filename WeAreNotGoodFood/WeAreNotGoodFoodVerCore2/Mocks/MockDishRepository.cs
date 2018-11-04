67f513c62c02e7673ad1d57490e09bde922ec343
jatisto <j.atisto@gmail.com>
2018-11-05 00:44:19 +0600

    AddMocksNotWork
    Добавил mocks пока не работают

---
+++ WeAreNotGoodFood/WeAreNotGoodFoodVerCore2/Mocks/MockDishRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using WeAreNotGoodFoodVerCore2.Interfaces;
+using WeAreNotGoodFoodVerCore2.Models;
+
+namespace WeAreNotGoodFoodVerCore2.Mocks
+{
+    public class MockDishRepository : IDishRepository
+    {
+        public IEnumerable<Dish> Dishes
+        {
+            get
+            {
+                return new List<Dish>
+                {
+                    new Dish
+                    {
+                        NameDish = "Пельмень жареный",
+                        ImagesDish = @"C:\Users\evgen\GitFolderHome\We_are_not_good-Food\WeAreNotGoodFood\WeAreNotGoodFoodVerCore2\wwwroot\images\Dish\загружено (3).jpg",
+                        Description = "Пельмень жареный с соусом morinaro",
+                        Price = 250,
+                        RestaurantId = 1
+                    },
+                    new Dish
+                    {
+                        NameDish = "Мясо по кавказски",
+                        ImagesDish = @"C:\Users\evgen\GitFolderHome\We_are_not_good-Food\WeAreNotGoodFood\WeAreNotGoodFoodVerCore2\wwwroot\images\Dish\00080202.jpg",
+                        Description = "С тушеными овощами отлично сочетается наше мясо гриль по-кавказски, которое представляет собой кусочки нежирного свиного",
+                        Price = 750,
+                        RestaurantId = 2
+                    },
+                    new Dish
+                    {
+                        NameDish = "Лапша китайская обыкновенная",
+                        ImagesDish = @"C:\Users\evgen\GitFolderHome\We_are_not_good-Food\WeAreNotGoodFood\WeAreNotGoodFoodVerCore2\wwwroot\images\Dish\images (7).jpg",
+                        Description = "лапша, лук, перец и т.д.",
+                        Price = 200,
+                        RestaurantId = 3
+                    }
+                };
+            }   
+    }
+
+    public IEnumerable<Dish> PreferredDishes {
+    get;
+    }
+    public Dish GetDisheById(int dishId)
+    {
+    throw new System.NotImplementedException();
+}
+
+}
+}
---
+++ WeAreNotGoodFood/WeAreNotGoodFoodVerCore2/Mocks/MockRestaurantRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using WeAreNotGoodFoodVerCore2.Interfaces;
+using WeAreNotGoodFoodVerCore2.Models;
+
+namespace WeAreNotGoodFoodVerCore2.Mocks
+{
+    public class MockRestaurantRepository : IRestaurantRepository
+    {
+        public IEnumerable<Restaurant> Restaurants
+        {
+            get
+            {
+                return new List<Restaurant>
+                {
+                    new Restaurant
+                    {
+                        ImagesRestaurant =
+                            @"C:\Users\evgen\GitFolderHome\We_are_not_good-Food\WeAreNotGoodFood\WeAreNotGoodFoodVerCore2\wwwroot\images\Restaurant\caption.jpg",
+                        Name = "Alcoholic",
+                        Description = "Уютно и приятно"
+                    },
+                    new Restaurant
+                    {
+                        ImagesRestaurant =
+                            @"C:\Users\evgen\GitFolderHome\We_are_not_good-Food\WeAreNotGoodFood\WeAreNotGoodFoodVerCore2\wwwroot\images\Restaurant\images (3).jpg",
+                        Name = "Не пьющий саловей",
+                        Description = "Если ты к нам пришёл то уже не забудеш"
+                    },
+                    new Restaurant
+                    {
+                        ImagesRestaurant =
+                            @"C:\Users\evgen\GitFolderHome\We_are_not_good-Food\WeAreNotGoodFood\WeAreNotGoodFoodVerCore2\wwwroot\images\Restaurant\images (5).jpg",
+                        Name = "Красивая жизнь",
+                        Description = "Ну это про нас!"
+                    },
+
+                };
+            }
+        }
+    }
+}
